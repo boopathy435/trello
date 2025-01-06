7b1bb9f327d9ccd49fc5cc6d00bb959834a554f5
Boopathy <boopathy.krishnasamy.ext@siemens-energy.com>
2025-01-06 22:06:02 +0530

    added the login and register controller

--- server/src/controllers/users.ts
+++ server/src/controllers/users.ts
@@ -1,14 +1,19 @@
 import { NextFunction, Request, Response } from "express";
-import UserModel from '../models/user';
+import UserModel from "../models/user";
 import { UserDocument } from "../types/user.interface";
+import { Error } from "mongoose";
+import jwt from "jsonwebtoken";
+import { secret } from "../config";
 
 const normalizeUser = (user: UserDocument) => {
-    return {
-        email: user.email,
-        username: user.username,
-        id: user.id
-    }
-}
+  const token = jwt.sign({ id: user.id, email: user.email }, secret);
+  return {
+    email: user.email,
+    username: user.username,
+    id: user.id,
+    token,
+  };
+};
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -18,8 +23,32 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
       password: req.body.password,
     });
     const savedUser = await newUser.save();
-    res.send(normalizeUser(savedUser))
+    res.send(normalizeUser(savedUser));
   } catch (err) {
+    if (err instanceof Error.ValidationError) {
+      const messages = Object.values(err.errors).map((err) => err.message);
+      res.status(422).json(messages);
+    }
     next(err);
   }
 };
+
+export const login = async (req: Request,res:Response,next:NextFunction) => {
+  try {
+    const user = await UserModel.findOne({email:req.body.email}).select("+password");
+    const errors = { emailOrPassword: "Incorrect email or password"}
+    if(!user){
+      res.status(422).json(errors)
+    }
+
+    const isSamePassword = await user?.validatePassword(req.body.password);
+
+    if(!isSamePassword){
+      res.status(422).json(errors)
+    }
+
+    res.send(normalizeUser(user as any));
+  } catch (err) {
+    next(err);
+  }
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,19 +3,22 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import mongoose from "mongoose";
 import * as usersController from "./controllers/users";
-import bodyParser from "body-parser"
+import bodyParser from "body-parser";
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
   res.send("API is UP");
 });
 
 app.post("/api/users", usersController.register);
+
+app.post("/api/users/login", usersController.login);
 
 io.on("connection", () => {
   console.log("connect");
